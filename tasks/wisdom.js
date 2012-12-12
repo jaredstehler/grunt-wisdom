bb939a2dd7b764e015b7a3abb53ca2682a184452
Jared Stehler <jared.stehler@cengage.com>
2012-12-12 10:44:25 -0500

    fix path issue

--- tasks/wisdom.js
+++ tasks/wisdom.js
@@ -1,6 +1,6 @@
 module.exports = function(grunt) {
   grunt.registerTask('wisdom', 'Wise Quotes', function() {
-    var defaults = require('./wisdom.json');
+    var defaults = require('../wisdom.json');
     
     var wiseness = grunt.config('wisdom') || defaults;
     var index = Math.floor(Math.random() * wiseness.length);
