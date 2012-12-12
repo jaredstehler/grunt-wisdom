3368a15d22d467038af127fef1aa016dc8c98133
Jared Stehler <jared.stehler@gmail.com>
2012-12-12 10:29:57 -0500

    Update tasks/compliment.js

--- tasks/compliment.js
+++ tasks/compliment.js
@@ -1,14 +1,10 @@
 module.exports = function(grunt) {
-  grunt.registerTask('compliment', 'Treat yo\' self!', function() {
-    var defaults = [
-      'You are so awesome!',
-      'You remind me of Brad Pitt, only you have a better body.',
-      'You are a funny, funny kid.'
-    ];
+  grunt.registerTask('wisdom', 'Wise Quotes', function() {
+    var defaults = require('./wisdom.json');
     
-    var compliments = grunt.config('compliment') || defaults;
-    var index = Math.floor(Math.random() * compliments.length);
+    var wiseness = grunt.config('wisdom') || defaults;
+    var index = Math.floor(Math.random() * wiseness.length);
     
-    grunt.log.writeln(compliments[index]);
+    grunt.log.writeln(wiseness[index]);
   });
 };
